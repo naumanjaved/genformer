1daa83a3f58127490e28d1c7fbc17ab385fd22e2
njaved <njaved@mit.edu>
2024-02-13 12:55:03 -0500

    add data zone to execution script

--- execute_sweep_atac_new.sh
+++ execute_sweep_atac_new.sh
@@ -11,8 +11,8 @@ python3 train_model_atac.py \
             --wandb_user="njaved" \
             --wandb_sweep_name="atac_pretraining" \
             --gcs_project="picard-testing-176520" \
-            --gcs_path="gs://genformer_europe_west_copy/atac_pretrain/524k/genformer_atac_pretrain_globalacc_conv_fpm" \
-            --gcs_path_holdout="gs://genformer_europe_west_copy/atac_pretrain/524k/genformer_atac_pretrain_globalacc_conv_fpm_valid" \
+            --gcs_path="gs://$3/atac_pretrain/524k/genformer_atac_pretrain_globalacc_conv_fpm" \
+            --gcs_path_holdout="gs://$3/atac_pretrain/524k/genformer_atac_pretrain_globalacc_conv_fpm_valid" \
             --input_length=524288 \
             --output_length=4096 \
             --output_length_ATAC=131072 \
@@ -24,7 +24,7 @@ python3 train_model_atac.py \
             --patience=50 \
             --output_res=128 \
             --min_delta=0.0000005 \
-            --model_save_dir="gs://genformer_europe_west_copy/atac_pretrain/models" \
+            --model_save_dir="gs://$3/atac_pretrain/models" \
             --epsilon=1.0e-8 \
             --num_transformer_layers="10" \
             --dropout_rate="0.20" \
