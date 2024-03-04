f5feb1fcb0542f0186c93ec314ded85035971d52
njaved <njaved@mit.edu>
2024-03-04 11:24:13 -0500

    add enformer test set code

--- enformer_baseline/execute_test.sh
+++ enformer_baseline/execute_test.sh
@@ -7,9 +7,7 @@ python3 model_eval.py \
             --wandb_user="njaved" \
             --wandb_sweep_name="enformer_rampage_ft_test" \
             --gcs_path_TSS="gs://enformer_baseline/human_tss/tfrecords" \
-            --epsilon=1.0e-8 \
             --num_parallel=4 \
-            --savefreq=1 \
             --test_examples=2032 \
             --num_targets=50 \
             --checkpoint_path="gs://enformer_baseline/models/enformer_baseline_2024-03-01_15:19:09_ENFORMER_LR1-1e-06_LR2-0.0001_GC-0.2_init-True_enformer_baseline_2024-03-01_15:19:00-28.data-00000-of-00001"
